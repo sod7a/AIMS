01b67f560dc41a64cb1ba27f37b856bcef5a75e2
sod7a <eggmayosushi0@gmail.com>
2026-05-07 23:32:32 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://postgres:1234@localhost:5432/Helpdesk'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-helpdesk-key'
     
